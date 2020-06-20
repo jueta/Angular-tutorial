efa0ce2cdc8202770b5dab365aa8ba7bfdd7583b
joao pedro <jpmm.brasil@hotmail.com>
2020-06-20 20:25:43 -0300

    Aprendendo Angular framework

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Jueta' },
+  { id: 12, name: 'Peppa Ian' },
+  { id: 13, name: 'Boli Night' },
+  { id: 14, name: 'Culas The Killa' },
+  { id: 15, name: 'Rodrogas' },
+  { id: 16, name: 'MarconhaPedraum' },
+  { id: 17, name: 'JhonnysMinas' },
+  { id: 18, name: 'Lelether' },
+  { id: 19, name: 'Sucker' },
+  { id: 20, name: 'Becky de havaianas' }
+];
